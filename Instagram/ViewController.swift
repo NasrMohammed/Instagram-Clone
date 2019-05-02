7e2c7ff66ac5ab2ef66cecaae407a4769b4a5063
NasrSyncBac <oshar2008@gmail.com>
2019-05-02 10:01:09 -0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Nasr Mohammed on 5/2/19.
+//  Copyright © 2019 Nasr Mohammed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
